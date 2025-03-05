3c61799a9abe191cc846ea1a74f498d1fa7b0080
DiegoIoI <Beeline.98dima@gmail.com>
2025-03-05 11:28:42 +0300

    Update site.py

--- функции_для_теста_ссылок/site.py
+++ функции_для_теста_ссылок/site.py
@@ -15,5 +15,5 @@ def index():
     return render_template('index.html', is_safe=None, message=None, url=None)
 
 
-if __name__ == '__main__':
+if __name__ == '__finalcheck__':
     app.run(debug=True)
